48babd164738e67a6c2f4f5aeeeafbdf760e7a75
Yakshit Poojary <yakshit.p@somaiya.edu>
2023-08-09 20:35:37 +0530

    Made footer and hotels

--- website/src/pages/home/Home.jsx
+++ website/src/pages/home/Home.jsx
@@ -1,7 +1,11 @@
+import "./home.css"
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured"
-import "./home.css"
+import PropertyList from "../../components/propertyList/PropertyList"
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties"
+import MailList from "../../components/mailList/MailList";
+import Footer from "../../components/footer/Footer";
 
 function Home(){
     return(
@@ -10,6 +14,12 @@ function Home(){
             <Header />
             <div className="homeContainer">
                 <Featured/>
+                <h1 className="homeTitle">Browse By Property Type</h1>
+                <PropertyList/>
+                <h1 className="homeTitle">Homes Guests Love</h1>
+                <FeaturedProperties />
+                <MailList />
+                <Footer />
             </div>
         </div>
     )
