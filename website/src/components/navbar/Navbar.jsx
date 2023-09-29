cfb6cb8664a82dfd4c9d615ee45fe5ef05128387
Yakshit Poojary <yakshit.p@somaiya.edu>
2023-09-29 11:29:48 +0530

    update

--- website/src/components/navbar/Navbar.jsx
+++ website/src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar(){
     return (  
         <div className="navbar">
             <div className="navContainer">
-                <span className="logo">lamabooking</span>
+                <span className="logo">CheckInnJoy</span>
                 <div className="navItems">
                     <button className="navButton">Register</button>
                     <button className="navButton">Login</button>
