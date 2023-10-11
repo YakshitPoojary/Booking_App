d69d927c18f761f0edef9e46df2adafd2be4cc88
Yakshit Poojary <yakshitpoojary2004@gmail.com>
2023-10-11 11:24:40 +0530

    Update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,33 +1,44 @@
-import "./navbar.css"
+import "./navbar.css";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
+  const handleRegister = () => {
+    navigate("/register");
+  };
 
-    const handleRegister = () => {
-      navigate("/register")
-    };
-        
-    const handleClick = () => {  
-      navigate("/login")
-    };
-    
-        
-    return (
-      
-      <div className="navbar">
-        <div className="navContainer">
-          <span className="logo">BookKaro</span>
-          <div className="navItems">
-            <button className="navButton" onClick={handleRegister}>Register</button>
-            <button className="navButton" onClick={handleClick}>Login</button>
+  const handleClick = () => {
+    navigate("/login");
+  };
+
+  const handleLogoClick = () => {
+    // Navigate to the home page when the logo is clicked
+    navigate("/");
+  };
+
+  return (
+    <div className="frame">
+      <a href="/" onClick={handleLogoClick} className="logo-link">
+        <div className="div">
+          <div className="overlap-group">
+            <div className="rectangle" />
+            <div className="text-wrapper">Karo</div>
           </div>
+          <div className="text-wrapper-2">Book</div>
         </div>
+      </a>
+
+      <div className="div-2">
+        <button className="div-wrapper" onClick={handleRegister}>
+          <div className="text-wrapper-4">Sign Up</div>
+        </button>
+        <button className="div-wrapper" onClick={handleClick}>
+          <div className="text-wrapper-4">Login</div>
+        </button>
       </div>
-      
-    )
+    </div>
+  );
+};
 
-}
-
-export default Navbar
+export default Navbar;
--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -81,7 +81,7 @@ const Hotel = () => {
         )}
         <div className="hotelWrapper">
           <button className="bookNow">Reserve or Book Now!</button>
-          <h1 className="hotelTitle">Tower Street Apartments</h1>
+          <h1 className="hotelTitle">Seven Hills Apartments</h1>
           <div className="hotelAddress">
             <FontAwesomeIcon icon={faLocationDot} />
             <span>Marol Andheri</span>
@@ -90,7 +90,7 @@ const Hotel = () => {
             Excellent location – 500m from center
           </span>
           <span className="hotelPriceHighlight">
-            Book a stay over $114 at this property and get a free airport taxi
+            Book a stay over Rs 1140 at this property and get a free airport taxi
           </span>
           <div className="hotelImages">
             {photos.map((photo, i) => (
@@ -108,33 +108,24 @@ const Hotel = () => {
             <div className="hotelDetailsTexts">
               <h1 className="hotelTitle">Stay in the heart of City</h1>
               <p className="hotelDesc">
-                Located a 5-minute walk from St. Florian's Gate in Krakow, Tower
-                Street Apartments has accommodations with air conditioning and
-                free WiFi. The units come with hardwood floors and feature a
-                fully equipped kitchenette with a microwave, a flat-screen TV,
-                and a private bathroom with shower and a hairdryer. A fridge is
-                also offered, as well as an electric tea pot and a coffee
-                machine. Popular points of interest near the apartment include
-                Cloth Hall, Main Market Square and Town Hall Tower. The nearest
-                airport is John Paul II International Kraków–Balice, 16.1 km
-                from Tower Street Apartments, and the property offers a paid
-                airport shuttle service.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat odio a est ea non esse reprehenderit recusandae sunt, voluptates, maxime sapiente consequuntur quasi, eveniet cum accusamus nostrum tempore praesentium excepturi.
               </p>
             </div>
             <div className="hotelDetailsPrice">
               <h1>Perfect for a 9-night stay!</h1>
               <span>
-                Located in the real heart of Krakow, this property has an
+                Located in the real heart of Marol, this property has an
                 excellent location score of 9.8!
               </span>
               <h2>
-                <b>$945</b> (9 nights)
+                <b>Rs 9450</b> (9 nights)
               </h2>
               <button>Reserve or Book Now!</button>
             </div>
           </div>
         </div>
       </div>
+      
     </div>
   );
 };
