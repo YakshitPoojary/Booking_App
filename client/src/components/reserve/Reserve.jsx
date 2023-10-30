bbcf614d79521c5f6873c4f4c648bb4d50415e72
Tejas Pundlik <tejaspundlik2@gmail.com>
2023-10-31 01:38:51 +0530

    Maybe

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -79,67 +79,69 @@ const Reserve = ({ setOpen, hotelId, price }) => {
 
 
     return (
-        <div className="reserve">
-            <div className="rContainer">
-                <FontAwesomeIcon
-                    icon={faCircleXmark}
-                    className="rClose"
-                    onClick={() => setOpen(false)}
-                />
-                <h3>Select your rooms:</h3>
-                {data.map((item) => (
-                    <div className="rItem" key={item._id}>
-                        <div className="rItemInfo">
-                            <div className="rTitle">{item.title}</div>
-                            <div className="rDesc">{item.desc}</div>
-                        </div>
-                        <div className="rSelectRooms">
-                            {item.roomNumbers.map((roomNumber) => (
-                                <div className="room">
-                                    <label>{roomNumber.number}</label>
-                                    <input
-                                        type="checkbox"
-                                        value={roomNumber._id}
-                                        onChange={handleSelect}
-                                        disabled={!isAvailable(roomNumber)}
-                                    />
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                ))}
-                {!showPayment &&
-                    <button className="rButton" onClick={handleClick}>
-                        Reserve Now!
-                    </button>
-                }
-                {showPayment && (
-                    <PayPalButtons
-                        createOrder={(data, actions) => {
-                            return actions.order.create({
-                                purchase_units: [
-                                    {
-                                        amount: {
-                                            value: price, // Replace with the total amount
-                                            currency_code: "USD", // Currency code
-                                        },
-                                    },
-                                ],
-                            });
-                        }}
-                        onApprove={(data, actions) => {
-                            return actions.order.capture().then(function (details) {
-                                afterPayment()
-                            });
-                        }}
-                        onError={(err) => {
-                            // Handle payment errors here
-                            console.error("Error during payment:", err);
-                        }}
+        <PayPalScriptProvider options={{ "client-id": "YOUR_PAYPAL_CLIENT_ID" }}>
+            <div className="reserve">
+                <div className="rContainer">
+                    <FontAwesomeIcon
+                        icon={faCircleXmark}
+                        className="rClose"
+                        onClick={() => setOpen(false)}
                     />
-                )}
+                    <h3>Select your rooms:</h3>
+                    {data.map((item) => (
+                        <div className="rItem" key={item._id}>
+                            <div className="rItemInfo">
+                                <div className="rTitle">{item.title}</div>
+                                <div className="rDesc">{item.desc}</div>
+                            </div>
+                            <div className="rSelectRooms">
+                                {item.roomNumbers.map((roomNumber) => (
+                                    <div className="room">
+                                        <label>{roomNumber.number}</label>
+                                        <input
+                                            type="checkbox"
+                                            value={roomNumber._id}
+                                            onChange={handleSelect}
+                                            disabled={!isAvailable(roomNumber)}
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                    {!showPayment &&
+                        <button className="rButton" onClick={handleClick}>
+                            Reserve Now!
+                        </button>
+                    }
+                    {showPayment && (
+                        <PayPalButtons
+                            createOrder={(data, actions) => {
+                                return actions.order.create({
+                                    purchase_units: [
+                                        {
+                                            amount: {
+                                                value: price, // Replace with the total amount
+                                                currency_code: "USD", // Currency code
+                                            },
+                                        },
+                                    ],
+                                });
+                            }}
+                            onApprove={(data, actions) => {
+                                return actions.order.capture().then(function (details) {
+                                    afterPayment()
+                                });
+                            }}
+                            onError={(err) => {
+                                // Handle payment errors here
+                                console.error("Error during payment:", err);
+                            }}
+                        />
+                    )}
+                </div>
             </div>
-        </div>
+        </PayPalScriptProvider>
     );
 };
 
