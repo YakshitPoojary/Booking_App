f18c5a387b180ab92b607ea5c6385605735ae27e
Tejas Pundlik <tejaspundlik2@gmail.com>
2023-10-31 00:38:15 +0530

    PayPal

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { SearchContextProvider } from './context/SearchContext';
 import { AuthContextProvider } from './context/AuthContext';
-
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <SearchContextProvider>
-        <App />
+        <PayPalScriptProvider options={{
+          "client-id": "AUviz0J32U7rwFdr-JaqKW5nTaNQxzixE78MUA7VxJd3CnDq9ofS0GpBwBtUWqfMHHeKm7MfgeG6B46q",
+          currency: "USD"
+        }}>
+          <App />
+        </PayPalScriptProvider>
       </SearchContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
